596d5787c7b3c21296491e232b8f670953df00cb
Oleksandr Konovalov <rassel1996@gmail.com>
2026-05-23 13:36:38 +0300

    fix: use relative server URL in OpenAPI spec, add CORS headers

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,12 @@ const swaggerDocument = JSON.parse(readFileSync(join(__dirname, 'openapi.json'),
 
 const app = express();
 app.use(express.json());
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  next();
+});
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (_req, res) => { res.redirect('/docs'); });
